e70d2f5453122cb4cbbc57425ef9fa4c247868e5
Jared Whalen <jared.m.whalen@gmail.com>
2025-04-09 22:00:26 -0400

    update github pages

--- src/App.svelte
+++ src/App.svelte
@@ -34,7 +34,12 @@
     103 million acres.
   </p>
   <Legend />
-  <p class="sr-only"></p>
+  <p class="sr-only">
+    Map showing the equivalent land area of U.S. wetlands in the 1780s (in
+    orange) and the 1980s (in gray), illustrating a dramatic reduction in
+    wetland coverage over time. The comparison is centered over the Midwest,
+    spanning from Nebraska to Kentucky.
+  </p>
 
   <div id="g-viz">
     <LossMap />
@@ -42,36 +47,34 @@
 
   {#if includeCredit}
     <div class="credit">
-      <div class="mapboxgl-ctrl-attrib-inner">
-        <a
-          href="https://www.mapbox.com/about/maps/"
-          target="_blank"
-          title="Mapbox"
-          aria-label="Mapbox">© Mapbox</a
-        >
-        <a
-          href="https://www.openstreetmap.org/copyright/"
-          target="_blank"
-          title="OpenStreetMap"
-          aria-label="OpenStreetMap">© OpenStreetMap</a
-        >
-        <a
-          class="mapbox-improve-map"
-          href="https://apps.mapbox.com/feedback/?owner=startribune&amp;id=cm8hhq3e6017901s55dfi5dmn&amp;access_token=pk.eyJ1Ijoic3RhcnRyaWJ1bmUiLCJhIjoiY2sxYjRnNjdqMGtjOTNjcGY1cHJmZDBoMiJ9.St9lE8qlWR5jIjkPYd3Wqw#/-92.53/39.55/5.06"
-          target="_blank"
-          title="Improve this map"
-          aria-label="Improve this map"
-          rel="noopener nofollow">Improve this map</a
-        >
-        <a
-          href="https://www.maxar.com/"
-          target="_blank"
-          title="Maxar"
-          aria-label="Maxar">© Maxar</a
-        >
-      </div>
       Data: U.S. Fish and Wildlife Service; Graphic by Jared Whalen /
-      <a target="_blank" href="https://agwaterdesk.org/">Ag & Water Desk</a>
+      <a target="_blank" href="https://agwaterdesk.org/">Ag & Water Desk</a><br/>
+      <a
+        href="https://www.mapbox.com/about/maps/"
+        target="_blank"
+        title="Mapbox"
+        aria-label="Mapbox">© Mapbox</a
+      >
+      <a
+        href="https://www.openstreetmap.org/copyright/"
+        target="_blank"
+        title="OpenStreetMap"
+        aria-label="OpenStreetMap">© OpenStreetMap</a
+      >
+      <a
+        class="mapbox-improve-map"
+        href="https://apps.mapbox.com/feedback/?owner=startribune&amp;id=cm8hhq3e6017901s55dfi5dmn&amp;access_token=pk.eyJ1Ijoic3RhcnRyaWJ1bmUiLCJhIjoiY2sxYjRnNjdqMGtjOTNjcGY1cHJmZDBoMiJ9.St9lE8qlWR5jIjkPYd3Wqw#/-92.53/39.55/5.06"
+        target="_blank"
+        title="Improve this map"
+        aria-label="Improve this map"
+        rel="noopener nofollow">Improve this map</a
+      >
+      <a
+        href="https://www.maxar.com/"
+        target="_blank"
+        title="Maxar"
+        aria-label="Maxar">© Maxar</a
+      >
     </div>
   {/if}
 </div>
